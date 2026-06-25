b18e1a782a90bf9e2b35bd7573602fdcfd0391d0
sarim ali <sarimshaikh597@gmail.com>
2026-06-26 03:20:06 +0500

    Delete frontend directory

--- frontend/src/socket.js
+++
@@ -1,9 +0,0 @@
-import { io } from "socket.io-client";
-
-const SERVER_URL = import.meta.env.VITE_SERVER_URL || "https://chatapp-aa9g.onrender.com/";
-
-// autoConnect is off so we only open the socket once the visitor has
-// picked a username (see App.jsx / JoinScreen.jsx).
-export const socket = io(SERVER_URL, {
-  autoConnect: false,
-});
